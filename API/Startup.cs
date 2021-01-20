ae2d6912758a14baf27dbdab45bced3ac6babe99
tanishq <tanishqsinghai11@gmail.com>
2021-01-20 16:00:15 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,7 +33,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddCors();            services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
@@ -52,7 +52,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
