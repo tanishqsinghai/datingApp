d1ed9efd04cad38e4561797f506d90a42719de32
tanishq <tanishqsinghai11@gmail.com>
2021-01-23 12:29:34 +0530

    End of Section 5

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -45,7 +45,7 @@ namespace API.Controllers
         }
         [HttpPost("login")]
 
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
             if (user == null) return Unauthorized("Invalid Username");
@@ -57,7 +57,11 @@ namespace API.Controllers
                 if (computedHash[i] != user.PasswordHash[i])
                     return Unauthorized("Invalid Password");
             }
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
 
         }
         private async Task<bool> UserExists(string username)
