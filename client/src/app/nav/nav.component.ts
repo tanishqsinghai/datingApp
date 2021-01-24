5db7123b7002e945d1469a674cfb46bb36547fd0
tanishq <tanishqsinghai11@gmail.com>
2021-01-24 20:35:30 +0530

    End of Section : 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -21,10 +21,6 @@ export class NavComponent implements OnInit {
   login(){
     this.accountService.login(this.model).subscribe(response =>{
       this.router.navigateByUrl('/members');
-    } , error => 
-    {
-          console.log(error);
-          this.toastr.error(error.error);
     })
   }
 
@@ -32,6 +28,4 @@ export class NavComponent implements OnInit {
     this.accountService.logout();
     this.router.navigateByUrl('/');
   }
-
-
 }
