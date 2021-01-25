bb80ee7c5a67edd0d7f2ae7786c6ddb77cc3f03e
tanishq <tanishqsinghai11@gmail.com>
2021-01-25 23:17:20 +0530

    End of section 9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { User } from './../_models/user';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -7,7 +8,7 @@ import { ReplaySubject } from 'rxjs';
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl='https://localhost:5001/api/';
+  baseUrl=environment.apiUrl;
   constructor(private http:HttpClient) { }
   private currentUserSource= new ReplaySubject<User>(1);
   currentUser$= this.currentUserSource.asObservable();
